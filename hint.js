9a9d03a516a2e2fa5754c2a0dad9796a0af54bdc
unknown <polyanin.04@mail.ru>
2024-02-07 18:27:10 +0300

    fixed tabs

---
+++ hint.js
@@ -0,0 +1,7 @@
+
+
+class Hint{
+    constructor() {
+        
+    }
+}
--- main.js
+++ main.js
@@ -2,13 +2,13 @@ const journalsTab = document.querySelector("#journals_tab");
 
 journalsTab.classList.add("tab_active");
 
-document.querySelectorAll(".second-panel>.tab").forEach((el) => {
+document.querySelectorAll(".second-panel__tab").forEach((el) => {
   el.addEventListener("click", () => {
     el.classList.add("tab_active");
   });
   document.addEventListener("click", (event) => {
     const isAnotherTabClicked = Array.from(
-      document.querySelectorAll(".second-panel>.tab")
+      document.querySelectorAll(".second-panel__tab")
     ).filter((el) => {
       return el.contains(event.target);
     }).length;
@@ -19,49 +19,35 @@ document.querySelectorAll(".second-panel>.tab").forEach((el) => {
   });
 });
 
-document
-  .querySelector(".journals")
-  .querySelectorAll(".tab")
-  .forEach((el, index) => {
-    if (index == 1) {
-      Array.from(el.children).forEach((child) => {
-        child.style.setProperty("color", "#ffe300");
-      });
-    }
-    el.onclick = () => {
-      const journalsTab = document.querySelector(".journals");
-      journalsTab.onmouseleave = () => {};
-      Array.from(el.children).forEach((child) => {
-        child.style.setProperty("color", "#ffe300");
-      });
-    };
+document.querySelectorAll(".third-panel__tab").forEach((el, index) => {
+  el.onclick = (event) => {
+    document.querySelectorAll(".third-panel__tab").forEach(el => {
+      el.querySelector('span').style.setProperty("color", "white");
+      el.querySelector('p').style.setProperty("color", "white");
+    })
+    console.log(event.target)
+    el.querySelector('span').style.setProperty("color", "#FFE200");
+    el.querySelector('p').style.setProperty("color", "#FFE200");
+  };
+});
 
-    document.addEventListener("click", (event) => {
-      const isAnotherTabClicked = Array.from(
-        document.querySelectorAll(".tabs-active>.tab")
-      ).filter((el) => {
-        return el.contains(event.target);
-      }).length;
-
-      if (!el.contains(event.target) && isAnotherTabClicked) {
-        Array.from(el.children).forEach((child) => {
-          child.style.setProperty("color", "white");
-        });
-      }
-    });
-  });
-
-const eventBus = new EventBus()
-eventBus.addEvent('deleteEverything')
-eventBus.addEvent('addVacancy')
-eventBus.addEvent('vacancyNameInput')
+const eventBus = new EventBus();
+eventBus.addEvent("deleteEverything");
+eventBus.addEvent("addVacancy");
+eventBus.addEvent("vacancyNameInput");
 const popupFactory = new PopupFactory();
 const controlPanel = new ControlPanel(popupFactory, eventBus);
-controlPanel.setup()
+controlPanel.setup();
 
 const filterQuery = new FilterQuery();
 const paginator = new Paginator();
-const table = new VacanciesTable({}, paginator, filterQuery, popupFactory, eventBus);
+const table = new VacanciesTable(
+  {},
+  paginator,
+  filterQuery,
+  popupFactory,
+  eventBus
+);
 table.setup();
 table.render();
 table.setupEventBusCallbacks();
